2be1bb44bd4959b0c6337ab80029a3a1bb8948d9
Alex Garel <alex@garel.org>
2022-09-22 10:25:21 +0200

    fix: higher timeout in import script

--- app/utils/connection.py
+++ app/utils/connection.py
@@ -5,5 +5,7 @@ import os
 from elasticsearch_dsl.connections import connections
 
 
-def get_connection():
-    return connections.create_connection(hosts=[os.getenv('ELASTICSEARCH_URL', '127.0.0.1:9200')])
+def get_connection(**kwargs):
+    return connections.create_connection(
+        hosts=[os.getenv('ELASTICSEARCH_URL', '127.0.0.1:9200')], **kwargs,
+    )
