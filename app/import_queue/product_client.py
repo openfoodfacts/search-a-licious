eeba4c418ad335fa73ff9fadc659e4157a20ba3f
Alex Garel <alex@garel.org>
2022-10-19 20:07:23 +0200

    fix: off url from env (#22)
    * build: use uvicorn reload in dev
    * fix: off url in env

--- app/import_queue/product_client.py
+++ app/import_queue/product_client.py
@@ -6,17 +6,15 @@ import requests
 
 
 class ProductClient:
+
     def __init__(self):
-        pass
+        self.server_url = os.getenv(
+            'OPENFOODFACTS_API_URL',
+            'https://world.openfoodfacts.org',
+        )
 
     def get_product(self, code):
-        url = '{}/api/v2/product/{}'.format(
-            os.getenv(
-                'OPENFOODFACTS_API_URL',
-                'https://world.openfoodfacts.org',
-            ),
-            code,
-        )
+        url = f'{self.server_url}/api/v2/product/{code}'
         response = requests.get(url)
         json_response = response.json()
         if not json_response or not json_response.get('product'):
