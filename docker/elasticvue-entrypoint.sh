bff5a8416168eb185b01797a8f6e48f2b6154cc9
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-09-22 10:40:49 +0200

    fix: fix bug in elasticvue entrypoint
    The custom configuration was added at every reload, which broke the
    config

--- docker/elasticvue-entrypoint.sh
+++ docker/elasticvue-entrypoint.sh
@@ -4,8 +4,8 @@
 # we would have done that with template mechanism,
 # but elasticvue use an lod nginx
 NGINX_CONF=/etc/nginx/conf.d/elasticvue.conf
-# prepare snipet to eventually add to conf
->/tmp/nginx_snipet
+# prepare snippet to eventually add to conf
+>/tmp/nginx_snippet
 CONF_MODIFIED=false
 if [ -n "$NGINX_BASIC_AUTH_USER_PASSWD" ] && ! ( grep auth_basic  $NGINX_CONF )
 then
@@ -14,17 +14,17 @@ then
     # create htpasswd file
     echo "$NGINX_BASIC_AUTH_USER_PASSWD" > /etc/nginx/elasticvue_htpasswd
     # change nginx config
-    cat >>/tmp/nginx_snipet <<END
+    cat >>/tmp/nginx_snippet <<END
     # add auth basic globally
     auth_basic "Off Search Elasticvue";
     auth_basic_user_file /etc/nginx/elasticvue_htpasswd;
 END
 fi
-if ! (grep "upstream elasticsearch" $NGINX_CONF)
+if ! (grep "location /es/ {" $NGINX_CONF)
 then
     CONF_MODIFIED=true
     echo "setting up ES access in nginx"
-    cat >>/tmp/nginx_snipet <<END
+    cat >>/tmp/nginx_snippet <<END
     # give access to ES through this url
 
     location /es/ {
@@ -45,7 +45,7 @@ END
 fi
 if [ "$CONF_MODIFIED" == "true" ]
 then
-    sed -i "/^ *access_log .*/r /tmp/nginx_snipet" $NGINX_CONF
+    sed -i "/^ *access_log .*/r /tmp/nginx_snippet" $NGINX_CONF
 fi
 
 exec "$@"
