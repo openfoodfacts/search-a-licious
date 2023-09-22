354019086be03cbe96f37dbebc09db40139bfe22
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-09-22 20:26:58 +0200

    feat: add possibility to set logging level in config

--- app/config.py
+++ app/config.py
@@ -4,6 +4,29 @@ from pydantic import BaseModel, Field, HttpUrl, model_validator
 from pydantic_settings import BaseSettings
 
 
+class LoggingLevel(StrEnum):
+    NOTSET = "NOTSET"
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+    def to_int(self):
+        if self is LoggingLevel.NOTSET:
+            return 0
+        elif self is LoggingLevel.DEBUG:
+            return 10
+        elif self is LoggingLevel.INFO:
+            return 20
+        elif self is LoggingLevel.WARNING:
+            return 30
+        elif self is LoggingLevel.ERROR:
+            return 40
+        elif self is LoggingLevel.CRITICAL:
+            return 50
+
+
 class Settings(BaseSettings):
     redis_expiration: int = 60 * 60 * 36  # 36h
     redis_reader_timeout: int = 5
@@ -12,6 +35,7 @@ class Settings(BaseSettings):
     # TODO: this should be in the config below
     openfoodfacts_base_url: str = "https://world.openfoodfacts.org"
     sentry_dns: str | None = None
+    log_level: LoggingLevel = LoggingLevel.INFO
 
 
 settings = Settings()
