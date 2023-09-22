9e9a624fdded6c00be7a18d997300af16547e7dd
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-09-22 10:52:54 +0200

    docs: improve API documentation

--- app/api.py
+++ app/api.py
@@ -10,25 +10,38 @@ from app.utils import connection, get_logger
 
 logger = get_logger()
 
-app = FastAPI()
+app = FastAPI(
+    title="search-a-licious API",
+    contact={
+        "name": "The Open Food Facts team",
+        "url": "https://world.openfoodfacts.org",
+        "email": "contact@openfoodfacts.org",
+    },
+    license_info={
+        "name": " AGPL-3.0",
+        "url": "https://www.gnu.org/licenses/agpl-3.0.en.html",
+    },
+)
 connection.get_connection()
 
 
 result_processor = load_result_processor(CONFIG)
 
 
-@app.get("/product/{barcode}")
-def get_product(barcode: str):
+@app.get("/document/{identifier}")
+def get_document(identifier: str):
+    """Fetch a document from Elasticsearch with specific ID."""
+    id_field_name = CONFIG.index.id_field_name
     results = (
         Search(index=CONFIG.index.name)
-        .query("term", code=barcode)
+        .query("term", **{id_field_name: identifier})
         .extra(size=1)
         .execute()
     )
     results_dict = [r.to_dict() for r in results]
 
     if not results_dict:
-        raise HTTPException(status_code=404, detail="Barcode not found")
+        raise HTTPException(status_code=404, detail="code not found")
 
     product = results_dict[0]
     return product
@@ -36,20 +49,64 @@ def get_product(barcode: str):
 
 @app.get("/search")
 def search(
-    q: str,
-    langs: Annotated[list[str] | None, Query()] = None,
-    num_results: int = 10,
-    projection: Annotated[list[str] | None, Query()] = None,
-    sort_by: str | None = None,
+    q: Annotated[
+        str,
+        Query(
+            description="""The search query, it supports Lucene search query
+syntax (https://lucene.apache.org/core/3_6_0/queryparsersyntax.html). Words
+that are not recognized by the lucene query parser are searched as full text
+search.
+
+Example: `categories:"en:beverages" strawberry brands:"casino"` query use a
+filter clause for categories and brands and look for "strawberry" in multiple
+fields.
+
+ The query is optional, but `sort_by` value must then be provided."""
+        ),
+    ] = None,
+    langs: Annotated[
+        list[str] | None,
+        Query(
+            description="""A list of languages we want to support during search. This
+list should include the user expected language, and additional languages (such
+as english for example).
+
+This is currently used for language-specific subfields to choose in which
+subfields we're searching in.
+
+If not provided, `['en']` is used."""
+        ),
+    ] = None,
+    num_results: Annotated[
+        int, Query(description="The number of results to return")
+    ] = 10,
+    fields: Annotated[
+        list[str] | None,
+        Query(
+            description="Fields to include in the response, all other fields will be ignored."
+        ),
+    ] = None,
+    sort_by: Annotated[
+        str | None,
+        Query(
+            description="""Field name to use to sort results, the field should exist
+            and be sortable. If it is not provided, results are sorted by descending relevance score."""
+        ),
+    ] = None,
 ):
+    if q is None and sort_by is None:
+        raise HTTPException(
+            status_code=400, detail="`sort_by` must be provided when `q` is missing"
+        )
+
     langs = set(langs or ["en"])
     query = build_search_query(
         q=q, langs=langs, num_results=num_results, config=CONFIG, sort_by=sort_by
     )
     results = query.execute()
 
-    projection_set = set(projection) if projection else None
-    response = result_processor.process(results, projection_set)
+    projection = set(fields) if fields else None
+    response = result_processor.process(results, projection)
 
     return {
         **response,
