74811150d150a302df09fe77ecdb2c30487050df
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-11-27 15:47:24 +0100

    feat: add support for synonyms for taxonomy field types
    Also add better documentation and unit tests

--- tests/unit/test_indexing.py
+++ tests/unit/test_indexing.py
@@ -1,6 +1,13 @@
 import pytest
 
-from app.indexing import process_text_lang_field
+from app.config import (
+    FieldConfig,
+    FieldType,
+    TaxonomyConfig,
+    TaxonomyIndexConfig,
+    TaxonomySourceConfig,
+)
+from app.indexing import process_taxonomy_field, process_text_lang_field
 
 
 @pytest.mark.parametrize(
@@ -26,7 +33,16 @@ from app.indexing import process_text_lang_field
                 "main": "MAIN",
                 "other": ["VN", "ID"],
             },
-        )
+        ),
+        # Same, but without main language
+        (
+            {
+                "product_name_fr": "FR",
+            },
+            "product_name",
+            False,
+            {"fr": "FR"},
+        ),
     ],
 )
 def test_process_text_lang_field(data, input_field, split, expected):
@@ -44,3 +60,121 @@ def test_process_text_lang_field(data, input_field, split, expected):
         )
         == expected
     )
+
+
+taxonomy_config = TaxonomyConfig(
+    sources=[
+        TaxonomySourceConfig(
+            name="category",
+            url="https://static.openfoodfacts.org/data/taxonomies/categories.full.json",
+        )
+    ],
+    exported_langs=["en"],
+    index=TaxonomyIndexConfig(),
+)
+
+
+@pytest.mark.parametrize(
+    "data, field, taxonomy_config, taxonomy_langs, expected",
+    [
+        (
+            {
+                "taxonomy_langs": ["fr", "it"],
+                # en:edamame has a "xx" name in the taxonomy
+                "categories_tags": "en:beverages,en:alcoholic-beverages,en:not-in-taxonomy,en:edamame",
+                # the original name should be saved under an `original` key
+                "categories": "Boissons,Boissons alcoolisées,Edamame",
+            },
+            FieldConfig(
+                type=FieldType.taxonomy,
+                name="categories",
+                input_field="categories_tags",
+                split=True,
+                add_taxonomy_synonyms=True,
+                taxonomy_name="category",
+            ),
+            taxonomy_config,
+            {"en"},
+            {
+                "fr": [
+                    "Boissons",
+                    "alcool",
+                    "alcools",
+                    "boisson alcoolisée",
+                    "Boissons alcoolisées",
+                    "Edamame",
+                ],
+                "it": ["Bevande", "Bevande alcoliche", "Edamame"],
+                "en": [
+                    "Drinks",
+                    "Beverages",
+                    "Alcoholic beverages",
+                    "drinks with alcohol",
+                    "alcohols",
+                    "Alcoholic drinks",
+                    "Edamame",
+                ],
+                "original": "Boissons,Boissons alcoolisées,Edamame",
+            },
+        ),
+        # Same, but without synonyms
+        (
+            {
+                "taxonomy_langs": ["fr", "it"],
+                "categories_tags": "en:beverages,en:alcoholic-beverages",
+            },
+            FieldConfig(
+                type=FieldType.taxonomy,
+                name="categories",
+                input_field="categories_tags",
+                split=True,
+                add_taxonomy_synonyms=False,
+                taxonomy_name="category",
+            ),
+            taxonomy_config,
+            {"en"},
+            {
+                "fr": [
+                    "Boissons",
+                    "Boissons alcoolisées",
+                ],
+                "it": ["Bevande", "Bevande alcoliche"],
+                "en": [
+                    "Beverages",
+                    "Alcoholic beverages",
+                ],
+            },
+        ),
+        # The field is missing here, we should return None
+        (
+            {"taxonomy_langs": ["fr", "it"]},
+            FieldConfig(
+                type=FieldType.taxonomy,
+                name="categories",
+                input_field="categories_tags",
+                split=True,
+                add_taxonomy_synonyms=False,
+                taxonomy_name="category",
+            ),
+            taxonomy_config,
+            {"en"},
+            None,
+        ),
+    ],
+)
+def test_process_taxonomy_field(data, field, taxonomy_config, taxonomy_langs, expected):
+    split_separator = ","
+    output = process_taxonomy_field(
+        data=data,
+        field=field,
+        taxonomy_config=taxonomy_config,
+        split_separator=split_separator,
+        taxonomy_langs=taxonomy_langs,
+    )
+
+    if expected is None:
+        assert output is None
+    else:
+        assert set(output.keys()) == set(expected.keys())
+        for key in expected.keys():
+            assert set(output[key]) == set(expected[key])
