3846235b4db176155d4da10eae23c92fac383323
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-11-27 16:03:39 +0100

    fix: fix isort issues

--- app/taxonomy.py
+++ app/taxonomy.py
@@ -7,8 +7,7 @@ import requests
 from app._types import JSONType
 from app.config import settings
 from app.utils import get_logger
-from app.utils.download import (download_file, http_session,
-                                should_download_file)
+from app.utils.download import download_file, http_session, should_download_file
 from app.utils.io import load_json
 
 DEFAULT_CACHE_DIR = settings.taxonomy_cache_dir.expanduser()
