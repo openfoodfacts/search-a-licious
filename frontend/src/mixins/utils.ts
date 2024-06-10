d788f1cb6d8c90f6bd8fcc3e00a8144a223af762
Kout95 <65901733+Kout95@users.noreply.github.com>
2024-06-10 12:06:13 +0200

    Update frontend/src/mixins/utils.ts
    Co-authored-by: Alex Garel <alex@garel.org>

--- frontend/src/mixins/utils.ts
+++ frontend/src/mixins/utils.ts
@@ -1,2 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+/**
+ * This mixin represents a mixin type
+ *
+ * We need it for every mixin we declare.
+ */
 export type Constructor<T = {}> = new (...args: any[]) => T;
