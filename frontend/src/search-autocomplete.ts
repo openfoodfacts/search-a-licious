895775bf5ccb201aa7b09226bb683824d3231752
Kout95 <antoine.bernier@outlook.fr>
2024-06-10 18:02:11 +0200

    docs: add comment to handleInput

--- frontend/src/search-autocomplete.ts
+++ frontend/src/search-autocomplete.ts
@@ -77,6 +77,8 @@ export class SearchaliciousAutocomplete extends DebounceMixin(LitElement) {
   handleInput(event: InputEvent) {
     const value = (event.target as HTMLInputElement).value;
     this.value = value;
+    // we don't need a very specific event name
+    // because it will be captured by the parent Facet element
     const inputEvent = new CustomEvent('autocomplete-input', {
       detail: {value: value},
       bubbles: true,
