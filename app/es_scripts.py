0197f991e80c23842098158f5c9a05c72499eeaf
Alex Garel <alex@garel.org>
2024-06-20 13:13:53 +0200

    feat: static_params for scripts
    Also fix a bug if sort_by is a script with minus in front,
    and create a real personal sort script
    and bettre display errors if storing scripts fails

--- app/es_scripts.py
+++ app/es_scripts.py
@@ -1,8 +1,12 @@
 """Module to manage ES scripts that can be used for personalized sorting
 """
 
+import elasticsearch
+
 from app import config
-from app.utils import connection
+from app.utils import connection, get_logger
+
+logger = get_logger(__name__)
 
 
 def get_script_prefix(index_id: str):
@@ -35,10 +39,11 @@ def _store_script(
     script_id: str, script: config.ScriptConfig, index_config: config.IndexConfig
 ):
     """Store a script in Elasticsearch."""
+    params = dict((script.params or {}), **(script.static_params or {}))
     payload = {
         "lang": script.lang.value,
         "source": script.source,
-        "params": script.params,
+        "params": params,
     }
     # hardcode context to scoring for the moment
     context = "score"
@@ -53,7 +58,17 @@ def sync_scripts(index_id: str, index_config: config.IndexConfig) -> dict[str, i
     # remove them
     _remove_scripts(current_ids, index_config)
     # store scripts
+    stored_scripts = 0
     if index_config.scripts:
         for script_id, script in index_config.scripts.items():
-            _store_script(get_script_id(index_id, script_id), script, index_config)
-    return {"removed": len(current_ids), "added": len(index_config.scripts or [])}
+            try:
+                _store_script(get_script_id(index_id, script_id), script, index_config)
+                stored_scripts += 1
+            except elasticsearch.ApiError as e:
+                logger.error(
+                    "Unable to store script %s, got exception %s: %s",
+                    script_id,
+                    e,
+                    e.body,
+                )
+    return {"removed": len(current_ids), "added": stored_scripts}
