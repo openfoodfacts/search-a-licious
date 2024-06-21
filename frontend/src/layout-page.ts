deac010ec2f901a17a07f647a291ca1f51592901
Valimp <valentinchauvet.contact@gmail.com>
2024-06-21 15:00:01 +0200

    fix: lint

--- frontend/src/layout-page.ts
+++ frontend/src/layout-page.ts
@@ -1,5 +1,5 @@
-import { LitElement, html, css } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
+import {LitElement, html, css} from 'lit';
+import {customElement, state} from 'lit/decorators.js';
 
 /**
  * Component for the layout of the page
@@ -7,62 +7,69 @@ import { customElement, state } from 'lit/decorators.js';
  */
 @customElement('layout-page')
 export class LayoutPage extends LitElement {
-    static override styles = css`
-        :host {
-        display: flex;
-        flex-direction: row;
-        gap: 20px;
-        }
-        .column {
-        flex: 1;
-        }
-        .col-1 {
-        flex-basis: 20%;
-        }
-        .col-2 {
-        flex-basis: 50%;
-        }
-        .col-3 {
-        flex-basis: 30%;
-        }
-        .col-1-no-graph {
-            flex-basis: 20%;
-        }
-        .col-2-no-graph {
-            flex-basis: 80%;
-        }
+  static override styles = css`
+    :host {
+      display: flex;
+      flex-direction: row;
+      gap: 20px;
+    }
+    .column {
+      flex: 1;
+    }
+    .col-1 {
+      flex-basis: 20%;
+    }
+    .col-2 {
+      flex-basis: 50%;
+    }
+    .col-3 {
+      flex-basis: 30%;
+    }
+    .col-1-no-graph {
+      flex-basis: 20%;
+    }
+    .col-2-no-graph {
+      flex-basis: 80%;
+    }
+  `;
+
+  /**
+   * Display graphs or not
+   */
+  @state()
+  displayGraphs = false;
+
+  override connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('toggle-graphs', (event: Event) =>
+      this.toggleGraphs(event)
+    );
+  }
+
+  override disconnectedCallback() {
+    window.removeEventListener('toggle-graphs', (event: Event) =>
+      this.toggleGraphs(event)
+    );
+    super.disconnectedCallback();
+  }
+
+  private toggleGraphs(_: Event) {
+    this.displayGraphs = !this.displayGraphs;
+  }
+
+  override render() {
+    return html`
+      <div class="column ${this.displayGraphs ? 'col-1' : 'col-1-no-graph'}">
+        <slot name="col-1"></slot>
+      </div>
+      <div class="column ${this.displayGraphs ? 'col-2' : 'col-2-no-graph'}">
+        <slot name="col-2"></slot>
+      </div>
+      ${this.displayGraphs
+        ? html`<div class="column col-3"><slot name="col-3"></slot></div>`
+        : ''}
     `;
-
-    /**
-     * Display graphs or not
-     */
-    @state()
-    displayGraphs = false;
-
-    override connectedCallback() {
-        super.connectedCallback();
-        window.addEventListener('toggle-graphs', (event: Event) => this.toggleGraphs(event));
-    }
-
-    override disconnectedCallback() {
-        window.removeEventListener('toggle-graphs', (event: Event) => this.toggleGraphs(event));
-        super.disconnectedCallback();
-    }
-
-    private toggleGraphs(_: Event) {
-        this.displayGraphs = !this.displayGraphs; 
-    }
-    
-    override render() {
-        return html`
-        <div class="column ${ this.displayGraphs ? 'col-1' : 'col-1-no-graph' }"><slot name="col-1"></slot></div>
-        <div class="column ${ this.displayGraphs ? 'col-2' : 'col-2-no-graph' }"><slot name="col-2"></slot></div>
-        ${this.displayGraphs ?
-            html`<div class="column col-3"><slot name="col-3"></slot></div>` :
-            ''
-        }
-        `;
-    }
+  }
 }
 
 declare global {
--- frontend/src/toggle-graphs.ts
+++ frontend/src/toggle-graphs.ts
@@ -1,20 +1,22 @@
-import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import {LitElement, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
 
 @customElement('toggle-graphs')
 export class ToggleGraphs extends LitElement {
-    private toggleGraphs() {
-        this.dispatchEvent(new CustomEvent('toggle-graphs', {
-            bubbles: true,
-            composed: true
-        }));
-    }
+  private toggleGraphs() {
+    this.dispatchEvent(
+      new CustomEvent('toggle-graphs', {
+        bubbles: true,
+        composed: true,
+      })
+    );
+  }
 
-    override render() {
-        return html`
-            <button part='button' @click="${this.toggleGraphs}">Show Graphs</button>
-        `;
-    }
+  override render() {
+    return html`
+      <button part="button" @click="${this.toggleGraphs}">Show Graphs</button>
+    `;
+  }
 }
 
 declare global {
