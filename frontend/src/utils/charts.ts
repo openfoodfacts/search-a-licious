2bc06c3f46b50d4458763327ccd53a4c7d3ad1a8
Kout95 <65901733+Kout95@users.noreply.github.com>
2024-07-10 09:38:28 +0200

    fix: move docs and fix content (#215)

--- frontend/src/utils/charts.ts
+++ frontend/src/utils/charts.ts
@@ -1,9 +1,8 @@
+/**
+ * Refresh vega charts by dispatching a resize event
+ * This is needed because vega charts are not displayed correctly because of the hidden sidebar
+ */
 export const refreshCharts = () => {
-  /**
-   * Refresh vega charts by dispatching a resize event
-   * This is needed because vega charts are not displayed correctly because of the hidden sidebar
-   * @private
-   */
   return setTimeout(() => {
     window.dispatchEvent(new Event('resize'));
   }, 0);
