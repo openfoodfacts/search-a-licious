f03be1da9b31c297c3ca7c32f3d635e772c62e43
Alex Garel <alex@garel.org>
2025-06-20 09:55:41 +0200

    test: make test output predictable
    To avoid changes between tests runs

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,12 +1,12 @@
+import json
 from pathlib import Path
 
-import orjson
 import pytest
 
 from app._types import JSONType
 from app.config import Config
 from app.query import build_elasticsearch_query_builder
-from app.utils.io import dump_json, load_json
+from app.utils.io import load_json
 
 DATA_DIR = Path(__file__).parent / "data"
 DEFAULT_CONFIG_PATH = DATA_DIR / "openfoodfacts_config.yml"
@@ -39,7 +39,8 @@ def load_expected_result(update_results):
 
     def load_expected_result_fn(test_id: str, data: JSONType):
         if update_results:
-            dump_json(DATA_DIR / f"{test_id}.json", data, option=orjson.OPT_INDENT_2)
+            with open(DATA_DIR / f"{test_id}.json", "w") as f:
+                json.dump(data, f, indent=2, sort_keys=True)
         elif not (DATA_DIR / f"{test_id}.json").exists():
             raise RuntimeError(
                 f"No result file for {test_id}, "
