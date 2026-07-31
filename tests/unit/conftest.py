cc6687cc3215a67c37ce3787046641c7d79351c7
Alex Garel <alex@garel.org>
2026-07-31 17:54:16 +0200

    test: fix unit tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 
+from app import config as app_config
 from app._types import JSONType
 from app.config import Config
 from app.query import build_elasticsearch_query_builder
@@ -11,12 +12,15 @@ from app.utils.io import load_json
 DATA_DIR = Path(__file__).parent / "data"
 DEFAULT_CONFIG_PATH = DATA_DIR / "openfoodfacts_config.yml"
 
-
 @pytest.fixture
 def default_config():
     """Fixture that returns default Open Food Facts index configuration for
     tests."""
-    yield Config.from_yaml(DEFAULT_CONFIG_PATH).indices["off"]
+    conf = Config.from_yaml(DEFAULT_CONFIG_PATH)
+    previous_conf = app_config._CONFIG
+    app_config._CONFIG = conf
+    yield conf.indices["off"]
+    app_config._CONFIG = previous_conf
 
 
 @pytest.fixture
@@ -30,6 +34,17 @@ def default_filter_query_builder(default_config):
     """Fixture that returns Luqum elasticsearch query builder based on default
     config."""
     yield build_elasticsearch_query_builder(default_config)
+
+
+@pytest.fixture
+def query_builder_cache_is_empty():
+    """Fixture that ensures the query builder cache is empty
+    before and after a test, so we don't get one from a previous test
+    """
+    from app.search import _ES_QUERY_BUILDERS
+    _ES_QUERY_BUILDERS.clear()
+    yield
+    _ES_QUERY_BUILDERS.clear()
 
 
 @pytest.fixture
